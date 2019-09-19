fe40729d7051142b00f1d5e53f5026ae79f50563
QzyUI <45230033+QzyUI@users.noreply.github.com>
2019-09-19 21:41:58 +0700

    Update planet.cpp

--- 5THLAB/planet.cpp
+++ 5THLAB/planet.cpp
@@ -24,45 +24,11 @@ void Planet::input()
 	cout << "\nНазвание: ";
 	getline(cin, name);
 	cout << "Диаметр: ";
-	bool fl = true; cin.get();
-	do
-	{
-		cin >> diam;
-		if (diam >= 0)
-			fl = false;
-		else cout << "Диаметр должна быть не отрицательной \nПовторите ввод диаметра:\n";
-	} while (fl);
-	if (diam == NULL)
-	{
-		throw "\nОшибка формата!";
-	}
+	cin >> diam;
 	cout << "Масса: ";
-	fl = true; cin.get();
-	do
-	{
-		cin >> mass;
-		if (mass >= 0)
-			fl = false;
-		else cout << "Масса должна быть не отрицательной \nПовторите ввод массы:\n";
-	} while (fl);
-	if (mass == NULL)
-	{
-		throw "\nОшибка формата!";
-	}
+	cin >> mass;
 	cout << "Дистанция: ";
-	fl = true; cin.get();
-	do
-	{
-		cin >> distance;
-		if (distance >= 0)
-			fl = false;
-		else cout << "Расстояние должна быть не отрицательной \nПовторите ввод дистаниции:\n";
-	} while (fl);
-	if (distance == NULL)
-	{
-		throw "\nОшибка формата!";
-	}
-	
+	cin >> distance;
 }
 void Planet::output()
 {
@@ -80,40 +46,7 @@ float Planet::getdistance()
 {
 	return distance;
 }
-void Planet::diskOut(ofstream& fout)
-{
-	fout << name << endl;
-	fout << diam << endl;
-	fout << mass << endl;
-	fout << distance << endl;
-} void Planet::diskIn(ifstream & fin)
-{
-	fin.get();
-	getline(fin, name);
-	fin.get();
-	fin >> diam;
-	if (diam < 0)
-		throw "Ошибка в содержимом файла: Диаметр не может быть отрицательной";
-	if (diam == NULL)
-	{
-		throw "\nОшибка в файле: Ошибка формата!";
-	}
-	fin.get();
-	fin >> mass;
-	if (mass < 0)
-		throw "Ошибка в содержимом файла: Масса не может быть отрицательной";
-	if (mass == NULL)
-	{
-		throw "\nОшибка в файле: Ошибка формата!";
-	}
-	fin.get();
-	fin >> distance;
-	if (distance < 0)
-		throw "Ошибка в содержимом файла: Расстояние не может быть отрицательной";
-	if (distance == NULL)
-	{
-		throw "\nОшибка в файле: Ошибка формата!";
-	}
+
 	
 }
 string Planet::getname()
