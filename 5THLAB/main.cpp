7ec51ec0cec2e296472b6377c04a606233c808f1
QzyUI <45230033+QzyUI@users.noreply.github.com>
2019-09-19 21:45:17 +0700

    Update main.cpp

--- 5THLAB/main.cpp
+++ 5THLAB/main.cpp
@@ -20,62 +20,14 @@ int compare(const void* s1, const void* s2) {
 int main()
 {    
 	setlocale(LC_ALL,"RUS");
-	int n; int p; bool fl = true;
+	int n; int p;
 	Planet* st = NULL;
-	try
-	{
-		do
-		{
-			cout << "Введите 1 или 2: ";
-			cout << "\n1 - Ввод списка планет и запись в файл";
-			cout << "\n2 - Чтение списка планет из файла";
-			cout << "\n>";
-			cin >> p;
-			cin.get();
-			if (p == NULL)
-			{
-				throw "\nОшибка формата!";
-			}
-			if (p == 1)
-			{
-				fl = false;
-				cout << "Введите кол-во планет: ";
-				cin >> n;
-				if (n == NULL)
-				{
-					throw "\nОшибка формата!";
-				}
-				st = new Planet[n];
-				cout << "Введите информацию о планетах:\n ";
-				for (int i = 0; i < n; i++)
-					st[i].input();
-				ofstream fout("D:\\Planet.txt");
-				fout << n << endl;
-				for (int i = 0; i < n; i++)
-					st[i].diskOut(fout);
-				fout.close();
-			}
-			if (p == 2)
-			{
-				fl = false;
-				char file_name[10];
-				cout << "Введите имя файла: ";
-				ifstream fin("D:\\Planet.txt");
-				if (!fin)
-					throw "\nОшибка: Файл не найден!";
-				else {
-					fin >> n;
-					if (n == NULL)
-					{
-						throw "\nОшибка в файле: Ошибка формата!";
-					}
-					st = new Planet[n];
-					for (int i = 0; i < n; i++)
-						st[i].diskIn(fin);
-					fin.close();
-				}
-			}
-		} while (fl);
+		cout << "Введите кол-во планет: ";
+		cin >> n;
+		st = new Planet[n];
+		cout << "Введите информацию о планетах:\n ";
+		for (int i = 0; i < n; i++)
+			st[i].input();
 		qsort(st, n, sizeof(Planet), compare);
 		cout << "Список планет: \n";
 		cout << left << setw(15) << "Название" << setw(15) << "Диаметр" << setw(10) << "Масса" << setw(10) << "Дистанция" << endl;
@@ -84,21 +36,7 @@ int main()
 			st[i].moddistance();
 			st[i].output();
 		}
-		float max = st[0].getdistance();
-		for(int i = 0; i < n; i++)
-			if (st[i].getdistance() > max)
-				max = st[i].getdistance();
-		cout << "Максимально удаленная планета: \n";
-		for(int i = 0; i<n; i++)
-			if (st[i].getdistance() == max) {
-				st[i].output();
-			}
 		delete [] st;
-	}
-	catch (const char* str)
-	{
-		cout << str << endl;
-	}
 	cout << "\n";
 	system("pause");
 }
